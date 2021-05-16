8611f565675e0f3b8fc688d2aab8bfc1ff4cfdca
Ilya Saenko <s41k4gg@gmail.com>
2021-05-16 18:48:49 +0300

    Add Class RandomEvents and some methods

--- Теор вер/Тест по теории вероятности/Тест по теории вероятности/Form1.cs
+++ Теор вер/Тест по теории вероятности/Тест по теории вероятности/Form1.cs
@@ -7,7 +7,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 using System.Windows.Forms;
+
 
 namespace Тест_по_теории_вероятности
 {
@@ -16,16 +18,16 @@ namespace Тест_по_теории_вероятности
         int question_count;//Счетчик вопросов
         int correct_answers;//Количество правильных ответов
         int wrong_answers;//Количество неправильных
-
+        int question_count_rand_ev;//Счетчик вопросов из 1й темы
         string[] array; //Массив данных
 
         int correct_answers_number; //номер правильного ответа
 
         int selected_response; //Номер выбранного ответа
-
+        
         StreamReader Reader; // Считывание из файла
 
-
+        List <RandomEvents> random_events;//Список вопросов
 
         public Test()
         {
@@ -35,13 +37,16 @@ namespace Тест_по_теории_вероятности
 
         void Start()
         {
-            var Encoding = System.Text.Encoding.GetEncoding(65001); //Подключаем Кириллицу
 
-            try
+
+            question_count = 0;
+            correct_answers = 0;
+            wrong_answers = 0;
+
+            array = new string[10]; //Создали массив для 10 вопросов
+
+            /*try
             {
-                Reader = new StreamReader(Directory.GetCurrentDirectory() + @"\test.txt",Encoding); //Обращаемся к нашему файлу с вопросами
-
-                this.Text = Reader.ReadLine();//Считываем название теста
 
                 question_count = 0;
                 correct_answers = 0;
@@ -55,34 +60,55 @@ namespace Тест_по_теории_вероятности
             catch(Exception)
             {
                 MessageBox.Show("Ошибка текстового файла");
-            }
-
+            }*/
+            AddQuestions();
             Quest();
         }
 
+        void AddQuestions()
+        {
+            var Encoding = System.Text.Encoding.GetEncoding(65001); //Подключаем Кириллицу
+            Reader = new StreamReader(Directory.GetCurrentDirectory() + @"\test.txt", Encoding); //Обращаемся к нашему файлу с вопросами
+            while(!Reader.EndOfStream)
+            {
+                string ex = Reader.ReadLine();//Считываем задание
+                string[] ans = new string[4];//Считываем массив ответов
+                for(int i = 0; i < ans.Length;i++)
+                    ans[i] = Reader.ReadLine();
+                int cor_ans = Convert.ToInt32(Reader.ReadLine());//Считываем правильный ответ
+                
+                random_events.Add(new RandomEvents(ex, ans, cor_ans));
+                question_count_rand_ev++;
+            }
+            Reader.Close();
+        }
         //Смена вопроса
         void Quest()
         {
-            label1.Text = Reader.ReadLine();
+            Random rnd = new Random();
+            int numb_ques = rnd.Next(0, question_count_rand_ev);
+
+            label1.Text = random_events[numb_ques].Question;
 
             //Варианты ответа
-            radioButton1.Text = Reader.ReadLine();
-            radioButton2.Text = Reader.ReadLine();
-            radioButton3.Text = Reader.ReadLine();
+            radioButton1.Text = random_events[numb_ques].Answers(0);
+            radioButton2.Text = random_events[numb_ques].Answers(1);
+            radioButton3.Text = random_events[numb_ques].Answers(2);
+            radioButton4.Text = random_events[numb_ques].Answers(3);
 
-            correct_answers_number = int.Parse(Reader.ReadLine()); // Считали правильный ответ
+            correct_answers_number = random_events[numb_ques].CorrectAnswer; // Считали правильный ответ
 
+            random_events.RemoveAt(numb_ques);
             radioButton1.Checked = false;
             radioButton2.Checked = false;
             radioButton3.Checked = false;
+            radioButton4.Checked = false;
 
             button1.Enabled = false;
             question_count++; //Увеличили число вопросов
 
-            if (Reader.EndOfStream == true)
+            if (question_count == 5)//Допилить
                 button1.Text = "Завершить";
-
-
 
         }
 
@@ -93,7 +119,7 @@ namespace Тест_по_теории_вероятности
             RadioButton p = (RadioButton)sender; //Проверяем процесс переключения
             var t = p.Name;
 
-            selected_response = int.Parse(t.Substring(11));
+            selected_response = int.Parse(t.Substring(11));//Тот вариант ответа, который выбрал пользователь(возвращает номоер кнопки введеной пользователем)
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -129,6 +155,7 @@ namespace Тест_по_теории_вероятности
                 wrong_answers++;
 
                 array[wrong_answers] = label1.Text;
+
             }
 
             if (button1.Text == "Начать заново")
@@ -138,6 +165,7 @@ namespace Тест_по_теории_вероятности
                 radioButton1.Visible = true;
                 radioButton2.Visible = true;
                 radioButton3.Visible = true;
+                radioButton4.Visible = true;
                 Start();
                 return;
             }
@@ -149,6 +177,7 @@ namespace Тест_по_теории_вероятности
                 radioButton1.Visible = false;
                 radioButton2.Visible = false;
                 radioButton3.Visible = false;
+                radioButton4.Visible = false;
 
                 label1.Text = string.Format("Тестирование завершено.\n" +
                     "Правильных ответов : {0} из {1}.\n" +
@@ -190,6 +219,7 @@ namespace Тест_по_теории_вероятности
             radioButton1.CheckedChanged += new EventHandler(Состояние_переключения);
             radioButton2.CheckedChanged += new EventHandler(Состояние_переключения);
             radioButton3.CheckedChanged += new EventHandler(Состояние_переключения);
+            radioButton4.CheckedChanged += new EventHandler(Состояние_переключения);
 
 
             Start();
